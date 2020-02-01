a34f5d7113cfbaa098e937e91eb88e412cdce80c
SirMatix <54940579+SirMatix@users.noreply.github.com>
2020-02-01 20:54:50 +0000

    Add variables

--- app/src/main/java/com/example/mobile_login/Register.java
+++ app/src/main/java/com/example/mobile_login/Register.java
@@ -6,32 +6,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
+
+    EditText mRegUsername, mRegPassword, mRegConfPassword, mRegEmail;
+    Button mRegister;
+    TextView loginHere;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        EditText mRegUsername, mRegPassword, mRegConfPassword, mRegEmail;
-        Button mRegSubmit;
-
         mRegUsername = findViewById(R.id.enterName);
         mRegEmail = findViewById(R.id.enterEmail);
         mRegPassword = findViewById(R.id.enterPass);
         mRegConfPassword = findViewById(R.id.confPass);
+        mRegister = findViewById(R.id.buttonSubmit);
+        loginHere = findViewById(R.id.loginHere);
 
-        mRegSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mRegUsername.getText();
-                mRegEmail.getText();
-                mRegPassword.getText();
-                mRegConfPassword.getText();
+        fAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
 
-            }
-        });
 
 
 
