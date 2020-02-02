14a97280d08f1950bff8652fe8d245ed7e0d2b77
SirMatix <54940579+SirMatix@users.noreply.github.com>
2020-02-02 23:42:04 +0000

    Register form

--- app/src/main/java/com/example/mobile_login/Register.java
+++ app/src/main/java/com/example/mobile_login/Register.java
@@ -47,7 +47,6 @@ public class Register extends AppCompatActivity {
 
 
 
-
         mRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
