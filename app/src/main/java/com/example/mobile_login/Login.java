b2a48f06401d698bebcc1d557eb76e5031d67aa9
SirMatix <54940579+SirMatix@users.noreply.github.com>
2020-02-04 11:55:23 +0000

    layout corrections

--- app/src/main/java/com/example/mobile_login/Login.java
+++ app/src/main/java/com/example/mobile_login/Login.java
@@ -12,9 +12,9 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
 
-    EditText mUsername, mPassword, mEmail;
-    Button mRegister;
-    TextView loginHere;
+    EditText mPassword, mEmail;
+    Button mLoginBtn;
+    TextView mCreateBtn;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
 
@@ -22,5 +22,15 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmail = findViewById(R.id.enterEmail);
+        mPassword = findViewById(R.id.enterPass);
+
+        mLoginBtn = findViewById(R.id.loginBtn);
+        mCreateBtn = findViewById(R.id.loginHere);
+
+        fAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
     }
 }
